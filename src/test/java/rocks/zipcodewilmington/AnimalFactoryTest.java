9875e966d4a07eeb2d57c5ca3393d6d9fcdd6e03
jd <j.judd.singer@gmail>
2022-07-12 22:51:11 -0400

    just starting to learn testing

--- src/test/java/rocks/zipcodewilmington/AnimalFactoryTest.java
+++ src/test/java/rocks/zipcodewilmington/AnimalFactoryTest.java
@@ -1,9 +1,54 @@
 package rocks.zipcodewilmington;
 
+import org.junit.Assert;
+import org.junit.Test;
+import rocks.zipcodewilmington.animals.Cat;
+import rocks.zipcodewilmington.animals.Dog;
+import rocks.zipcodewilmington.animals.animal_creation.AnimalFactory;
+import rocks.zipcodewilmington.animals.animal_storage.CatHouse;
+import rocks.zipcodewilmington.animals.animal_storage.DogHouse;
+
+import java.util.Date;
+
 /**
  * @author leon on 4/19/18.
  */
 public class AnimalFactoryTest {
+
+    @Test
+    public void createDogTest(){
+        Integer expectedId = DogHouse.getNumberOfDogs();
+        Date expectedDate = new Date();
+        String expectedName = "Shilah";
+
+        Dog dog = AnimalFactory.createDog(expectedName, expectedDate);
+
+        Integer actualId = dog.getId();
+        Date actualDate = dog.getBirthDate();
+        String actualName = dog.getName();
+
+        Assert.assertEquals(expectedId, actualId);
+        Assert.assertEquals(expectedDate, actualDate);
+        Assert.assertEquals(expectedName, actualName);
+
+    }
+@Test
+    public void createCatTest(){
+        Integer expectedId = CatHouse.getNumberOfCats();
+        Date expectedDate = new Date();
+        String expectedName = "mikko";
+
+        Cat cat = AnimalFactory.createCat(expectedName, expectedDate);
+
+        Integer actualId = cat.getId();
+        Date actualDate = cat.getBirthDate();
+        String actualName = cat.getName();
+
+        Assert.assertEquals(expectedId, actualId);
+        Assert.assertEquals(expectedDate, actualDate);
+        Assert.assertEquals(expectedName, actualName);
+
+    }
     //TODO - Create Test for `Animal createDog(String name, Date birthDate)`
     //TODO - Create Test for `Animal createCat(String name, Date birthDate)`
 }
--- src/test/java/rocks/zipcodewilmington/CatTest.java
+++ src/test/java/rocks/zipcodewilmington/CatTest.java
@@ -3,13 +3,28 @@ package rocks.zipcodewilmington;
 import org.junit.Assert;
 import org.junit.Test;
 import rocks.zipcodewilmington.animals.Cat;
+import rocks.zipcodewilmington.animals.animal_creation.AnimalFactory;
+import rocks.zipcodewilmington.animals.animal_storage.CatHouse;
 
 import java.util.Date;
 
 /**
  * @author leon on 4/19/18.
  */
+@Test
 public class CatTest {
+    String expectedName = "kittyPurry";
+
+    Cat cat = AnimalFactory.createCat(expectedName);
+
+    String actualName = .getName();
+
+
+    Assert.assertEquals(expectedName,actualName);
+     Assert.assertEquals(expectedBirthDate,actualBirthDate);
+        Assert.assertEquals(actualId,actualId);
+}
+
     // TODO - Create tests for `void setName(String name)`
     // TODO - Create tests for `speak`
     // TODO - Create tests for `setBirthDate(Date birthDate)`
